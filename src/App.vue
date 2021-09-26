a5416118a22b6792000261a23b6ae68c65ccdb21
Pavel Yakupov <pavel.yakupov@software-kit.ru>
2021-09-26 15:59:28 +0300

    asdad

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,20 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <hello-world/>
     </div>
     <router-view/>
   </div>
 </template>
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+export default {
+  components:{
+    HelloWorld
+  }
+}
+</script>
+
 
 <style>
 #app {
